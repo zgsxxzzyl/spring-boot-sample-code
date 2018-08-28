35de1cd231fdf4a954dd7799c68713a07ac1c87a
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-08-28 20:13:48 +0800

    init global exception

---
+++ springboot-global-exception/src/main/java/com/example/BaseController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @RequestMapping(value="/ex")
+    @ResponseBody
+    public String error(){
+        int i=5/0;
+        return "ex";
+    }
+}
