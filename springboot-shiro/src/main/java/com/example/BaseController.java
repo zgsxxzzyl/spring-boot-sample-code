3865f3b93dd46b04ab1255790cf241469218091d
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-08-30 17:03:20 +0800

    来了一个高难度bug，解决完没时间学习了

---
+++ springboot-shiro/src/main/java/com/example/BaseController.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class BaseController {
+
+    @RequestMapping({"/","/index"})
+    public String index(){
+        Subject subject = SecurityUtils.getSubject();
+        subject.getSession().setAttribute("user","mabh");
+
+        if(!subject.isAuthenticated()){
+            UsernamePasswordToken token = new UsernamePasswordToken("user","mabh");
+            token.setRememberMe(true);
+            subject.login(token);
+        }
+        return "/index";
+    }
+
+    public boolean validate(){
+
+        return false;
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(){
+
+        return "/login";
+    }
+}
