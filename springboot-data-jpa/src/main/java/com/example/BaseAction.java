b9777104c7b5e5a810634c540f0249e351d0259d
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-09-25 16:20:35 +0800

    two datasource jpa demo

---
+++ springboot-data-jpa/src/main/java/com/example/BaseAction.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BaseAction {
+
+    @Autowired
+    @Qualifier("masterJdbcTemplate")
+    public JdbcTemplate masterJdbcTemplate;
+
+    @Autowired
+    @Qualifier("slave1JdbcTemplate")
+    public JdbcTemplate slave1JdbcTemplate;
+
+    @RequestMapping("a")
+    public List<User> master(){
+        return masterJdbcTemplate.query("SELECT * FROM USER",new UserRowMapper());
+    }
+
+    @RequestMapping("b")
+    public List<User> slave1(){
+        return slave1JdbcTemplate.query("SELECT * FROM USER",new UserRowMapper());
+    }
+}
