da5355fae8d284f94a9f7163c44707ce329d0617
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-09-27 21:01:41 +0800

    springboot mybatis aop配置动态切换数据源

---
+++ springboot-dynamic-datasource/src/main/java/com/example/dao/support/DynamicDataSource.java
@@ -0,0 +1,10 @@
+package com.example.dao.support;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DynamicDataSourceHolder.getDataSource();
+    }
+}
