f062450d294b65974e33f1d115a42b94f5e7eff6
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-09-29 14:49:59 +0800

    增加一些调试方法，获取加载的全部bean、注解

--- springboot-dynamic-datasource/src/main/java/com/example/SpringbootDynamicDatasourceApplication.java
+++ springboot-dynamic-datasource/src/main/java/com/example/SpringbootDynamicDatasourceApplication.java
@@ -2,11 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringbootDynamicDatasourceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDynamicDatasourceApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringbootDynamicDatasourceApplication.class, args);
+        // 获取加载的全部bean
+        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+        // 获取加载指定注解的全部bean
+        String[] beanNamesForAnnotation = run.getBeanNamesForAnnotation(RestController.class);
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
     }
 }
