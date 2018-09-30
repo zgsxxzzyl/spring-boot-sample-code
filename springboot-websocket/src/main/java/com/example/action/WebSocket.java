31dbcf7d064d0385c5a5ce79a340851d31403c44
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-09-30 15:04:32 +0800

    update springboot-websocket

--- springboot-websocket/src/main/java/com/example/action/WebSocket.java
+++ springboot-websocket/src/main/java/com/example/action/WebSocket.java
@@ -5,16 +5,34 @@ import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
 @ServerEndpoint(value = "/websocket")
 public class WebSocket {
     private Session session;
+    private static int count = 0;
+    private static CopyOnWriteArraySet<WebSocket> webSocketSet = new CopyOnWriteArraySet<WebSocket>();
+
 
     @OnOpen
     public void onOpen(Session session) {
         this.session = session;
+        webSocketSet.add(this);
+        addOnlineCount();
         System.out.println("--> onOpen");
+    }
+
+    private void addOnlineCount() {
+        count++;
+        System.out.println("--> Count : " + count);
+        sendMessageAll(String.valueOf(count));
+    }
+
+    private void removeOnlineCount() {
+        count--;
+        System.out.println("--> Count : " + count);
+        sendMessageAll(String.valueOf(count));
     }
 
     @OnMessage
@@ -26,7 +44,7 @@ public class WebSocket {
     @OnClose
     public void onClose() {
         System.out.println("--> onClose");
-
+        webSocketSet.remove(this);
     }
 
     @OnError
@@ -42,4 +60,11 @@ public class WebSocket {
             e.printStackTrace();
         }
     }
+
+    private void sendMessageAll(String message) {
+        for (WebSocket webSocket : webSocketSet) {
+            webSocket.sendMessage(message);
+        }
+    }
+    
 }
