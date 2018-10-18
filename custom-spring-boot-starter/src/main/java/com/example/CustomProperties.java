fc8086eb8f09dea4ef58a04de8f8c180652a825b
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-10-18 17:08:55 +0800

    update

--- custom-spring-boot-starter/src/main/java/com/example/CustomProperties.java
+++ custom-spring-boot-starter/src/main/java/com/example/CustomProperties.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "custom")
 public class CustomProperties {
+
     private String name;
 
     public String getName() {
--- custom-spring-boot-starter/src/main/java/com/example/CustomService.java
+++ custom-spring-boot-starter/src/main/java/com/example/CustomService.java
@@ -1,12 +1,13 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
+//@Component
 public class CustomService {
-    @Autowired
+    //    @Autowired
     private CustomProperties customProperties;
+
+    public CustomService() {
+        System.out.println("");
+    }
 
     public String name() {
         return customProperties.getName();
