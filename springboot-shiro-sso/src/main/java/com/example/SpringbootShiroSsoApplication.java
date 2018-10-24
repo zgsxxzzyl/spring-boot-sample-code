9e77b7ccd42bcde73a1df7c1712a962afa3c4fd4
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-10-24 18:03:24 +0800

    add sso

---
+++ springboot-shiro-sso/src/main/java/com/example/SpringbootShiroSsoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroSsoApplication.class, args);
+    }
+}
