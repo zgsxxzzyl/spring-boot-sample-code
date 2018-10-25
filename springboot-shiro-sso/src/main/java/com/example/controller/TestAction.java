4c961bb28cc92caf004cd027abfc1a6d8878b5d5
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-10-25 20:59:11 +0800

    update

--- springboot-shiro-sso/src/main/java/com/example/controller/TestAction.java
+++ springboot-shiro-sso/src/main/java/com/example/controller/TestAction.java
@@ -6,13 +6,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestAction {
 
-    @RequestMapping("de")
+    @RequestMapping("/de")   //pass
     public String demo() {
-        return "redirect:detail.html";
+        return "redirect:/detail.html";
     }
 
-    @RequestMapping("de2")
+    @RequestMapping("de4")      //pass
+    public String demo4() {
+        return "forward:/detail.html";
+    }
+
+    @RequestMapping("de5")
+    public String demo5() {
+        return "forward:/detail";
+    }
+
+    @RequestMapping("de6")      //pass
+    public String demo6() {
+        return "forward:/de";
+    }
+
+    @RequestMapping("de2")      //pass
     public String demo1() {
         return "demo";
+    }
+
+    @RequestMapping("de3")
+    public String demo3() {
+        return "detail";
     }
 }
