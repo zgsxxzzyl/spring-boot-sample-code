635fc3512301783ba6b07074c40f6b5142d30ab9
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-11-22 16:57:16 +0800

    update

--- springboot-jpa-datasource/src/main/java/com/SpringbootJpaDatasourceApplication.java
+++ springboot-jpa-datasource/src/main/java/com/SpringbootJpaDatasourceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootDataJpaApplication {
+public class SpringbootJpaDatasourceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDataJpaApplication.class, args);
+        SpringApplication.run(SpringbootJpaDatasourceApplication.class, args);
     }
 }
