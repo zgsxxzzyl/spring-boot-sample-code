c38f0b584656ea9d7fcf4040f09525b9dc3884bb
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2018-11-29 19:52:59 +0800

    update

--- springboot-multi-sample/README.md
+++ springboot-multi-sample/README.md
@@ -1,1 +1,3 @@
 [Spring中的后置处理器BeanPostProcessor讲解](https://www.cnblogs.com/sishang/p/6576665.html)
+
+BeanFactoryPostProcessor
---
+++ springboot-multi-sample/src/main/java/com/CustomBeanFactoryPostProcessor.java
@@ -0,0 +1,26 @@
+package com;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+import java.util.Iterator;
+
+public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("调用了自定义的BeanFactoryPostProcessor " + beanFactory);
+        Iterator it = beanFactory.getBeanNamesIterator();
+
+        String[] names = beanFactory.getBeanDefinitionNames();
+        // 获取了所有的bean名称列表
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+
+            BeanDefinition bd = beanFactory.getBeanDefinition(name);
+            System.out.println(name + " bean properties: " + bd.getPropertyValues().toString());
+            // 本内容只是个demo，打印持有的bean的属性情况
+        }
+    }
+}
