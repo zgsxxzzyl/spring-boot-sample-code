171a566bc33c66b3fae8c638dadc96cd55943f53
zgsxxzzyl <zgsxxzzyl@hotmail.com>
2019-02-18 14:46:12 +0800

    update

--- springboot-web-thymeleaf/src/main/java/com/SpringbootWebThymeleafApplication.java
+++ springboot-web-thymeleaf/src/main/java/com/SpringbootWebThymeleafApplication.java
@@ -2,18 +2,17 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ServletComponentScan
+//@ServletComponentScan
+//public class SpringbootWebThymeleafApplication implements WebMvcConfigurer {
+
 @SpringBootApplication
-public class SpringbootWebThymeleafApplication implements WebMvcConfigurer {
+public class SpringbootWebThymeleafApplication {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/inter/**").excludePathPatterns("/ca/**");
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/inter/**").excludePathPatterns("/ca/**");
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebThymeleafApplication.class, args);
