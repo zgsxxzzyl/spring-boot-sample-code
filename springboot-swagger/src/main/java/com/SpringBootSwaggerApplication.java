abcddccc3cb274dd69f90675901187f9b6d3ef68
zgsxxzzyl@hotmail.com <zgsxxzzyl@hotmail.com>
2020-12-01 11:36:41 +0800

    20201201

--- springboot-swagger/src/main/java/com/SpringBootSwaggerApplication.java
+++ springboot-swagger/src/main/java/com/SpringBootSwaggerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * http://127.0.0.1:8080/swagger-ui.html
+ */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringBootSwaggerApplication {
 
